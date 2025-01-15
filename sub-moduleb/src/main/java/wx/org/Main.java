0528f6e9600b6d1a4084e836eb03f6eccd725230
WUXIN <myeagleandsnake@gmail.com>
2025-01-15 17:42:56 +0800

    feat

---
+++ sub-moduleb/src/main/java/wx/org/Main.java
@@ -0,0 +1,8 @@
+package wx.org;
+
+/**
+ * @author wuxin
+ * @date 2025/01/15 17:41:04
+ */
+public class Main {
+}
---
+++ sub-moduleb/src/main/java/wx/org/module22/modulebbb111.java
@@ -0,0 +1,8 @@
+package wx.org.module22;
+
+/**
+ * @author wuxin
+ * @date 2025/01/14 17:27:32
+ */
+public class modulebbb111 {
+}
